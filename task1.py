f237446594e916aa67fd383862bb0554034a31dc
ink404dot <94466285+ink404dot@users.noreply.github.com>
2024-09-06 14:19:14 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,14 +1,20 @@
 from datetime import datetime, date
+import sys
+
 
 def get_days_from_today(date_str):
     # Перетворюємо рядок у об'єкт datetime
-    target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+    try:
+        target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
+    except Exception as e:
+        sys.exit(e)
     # Отримуємо поточну дату
     today = date.today()
     # Обчислюємо різницю між датами
     delta = target_date - today
     # Повертаємо кількість днів
     return delta.days
+
 
 # Приклад використання:
 print(get_days_from_today("2021-10-09"))  # Якщо сьогодні 5 травня 2021 року, результат: 157
